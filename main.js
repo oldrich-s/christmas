da09d9766fa794f84e86cd69c0d8c3194ce637d5
Oldrich Svec <oldrich.svec@gmail.com>
2024-12-09 13:59:12 +0100

    fixes

--- main.js
+++ main.js
@@ -7,34 +7,34 @@ const player = require('play-sound')();
 const app = express();
 app.use(bodyParser.json());
 
-let targetDate = null;
+let job = null;
 
 app.use(express.static('public'));
 
 app.post('/set-timer', (req, res) => {
+    if (job) job.cancel();
+
     const { targetTime, range } = req.body;
     const [hours, minutes] = targetTime.split(':').map(Number);
     const rangeInMinutes = Number(range);
 
     const randomOffset = Math.floor((Math.random() * 2 - 1) * rangeInMinutes);
 
-    targetDate = new Date();
+    const targetDate = new Date();
     targetDate.setHours(hours, minutes + randomOffset, 0, 0);
 
-    schedule.scheduleJob(targetDate, () => {
+    job = schedule.scheduleJob(targetDate, () => {
         axios.get('http://192.168.1.254/rpc/Switch.Set?id=0&on=true')
-        player.play(`./hudba/*.mp3`);
-
-        targetDate = null;
+        //player.play(`./hudba/*.mp3`);
     });
 
     res.sendStatus(200);
 });
 
 app.get('/next-execution', (req, res) => {
-    res.json({ targetDate: targetDate.toISOString() });
+    res.json({ targetDate: job ? job.nextInvocation() : null });
 });
 
 app.listen(3000, () => {
-    console.log(`Server běží na portu ${PORT}`);
+    console.log(`Server běží na portu 3000. Zapni Bluetooth audio!!!`);
 });
